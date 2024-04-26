0a73587db685ad328e5d75313c1612f0a41b795e
Patrick Wang <wang.patrick57@gmail.com>
2024-04-26 19:24:30 -0400

    22-25

---
+++ cardest_joblight_queries/25a.sql
@@ -0,0 +1,1 @@
+SELECT * FROM title t,movie_keyword mk,movie_companies mc,movie_info mi WHERE t.id=mk.movie_id AND t.id=mc.movie_id AND t.id=mi.movie_id AND mk.keyword_id=398 AND mc.company_type_id=2 AND t.production_year=1998;

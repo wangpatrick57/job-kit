ccf0059e40cde8586b6c926a50706a9b7eda89a5
Patrick Wang <wang.patrick57@gmail.com>
2024-04-29 15:49:01 -0400

    fmt 1c and 1d

--- cardest_joblight_queries/1c.sql
+++ cardest_joblight_queries/1c.sql
@@ -1,1 +1,9 @@
-SELECT * FROM movie_companies mc,title t,movie_info_idx mi_idx WHERE t.id=mc.movie_id AND t.id=mi_idx.movie_id AND mi_idx.info_type_id=112 AND mc.company_type_id=2 AND t.production_year>2010;
+SELECT *
+FROM movie_companies mc,
+    title t,
+    movie_info_idx mi_idx
+WHERE t.id=mc.movie_id
+    AND t.id=mi_idx.movie_id
+    AND mi_idx.info_type_id=112
+    AND mc.company_type_id=2
+    AND t.production_year>2010;

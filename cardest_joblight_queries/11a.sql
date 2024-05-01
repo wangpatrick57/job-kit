3a89a66fc1d2f1eb1b1c308dd44747978ce2ec4c
Patrick Wang <wang.patrick57@gmail.com>
2024-04-30 20:53:00 -0400

    more fmt

--- cardest_joblight_queries/11a.sql
+++ cardest_joblight_queries/11a.sql
@@ -1,1 +1,10 @@
-SELECT * FROM title t,movie_keyword mk,movie_companies mc WHERE t.id=mk.movie_id AND t.id=mc.movie_id AND mk.keyword_id=398 AND mc.company_type_id=2 AND t.production_year>1950 AND t.production_year<2000;
+SELECT *
+FROM title t,
+    movie_keyword mk,
+    movie_companies mc
+WHERE t.id=mk.movie_id
+    AND t.id=mc.movie_id
+    AND mk.keyword_id=398
+    AND mc.company_type_id=2
+    AND t.production_year>1950
+    AND t.production_year<2000;
--- cardest_joblight_queries/11c.sql
+++ cardest_joblight_queries/11c.sql
@@ -1,1 +1,7 @@
-SELECT * FROM title t,movie_keyword mk,movie_companies mc WHERE t.id=mk.movie_id AND t.id=mc.movie_id AND t.production_year>1950;
+SELECT *
+FROM title t,
+    movie_keyword mk,
+    movie_companies mc
+WHERE t.id=mk.movie_id
+    AND t.id=mc.movie_id
+    AND t.production_year>1950;
